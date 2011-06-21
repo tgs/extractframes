b030e2b8979b7253a0ce1578630902faf0267f12
Thomas Grenfell Smith <thomathom@gmail.com>
2011-06-21 15:47:57 -0400

    make it classy

--- extractframes/multirange.py
+++ extractframes/multirange.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python
+import itertools
 
+class multirange(object):
+    def __init__(self, range_str):
+        """Take string like '1-10,12-40', and return the indicated integers"""
+        
+        sections = range_str.split(',')
+        self.ranges = []
+        last_end = float('-inf')
+        for section in sections:
+            parts = [int(x) for x in section.split('-')]
 
-def multirange(rangestr):
-    """Take string like '1-10,12-40', and return the indicated integers"""
-    parts = range_str.split('-')
-    if len(parts) != 2:
-        raise ValueError('Ranges must have exactly two parts, "start-end"')
-    return xrange(parts[0], parts[1] + 1)
+            if parts[0] < last_end:
+                raise ValueError('Ranges must not overlap')
+            if len(parts) != 2:
+                raise ValueError('Ranges must have exactly two parts, "start-end"')
+
+            self.ranges.append(xrange(parts[0], parts[1] + 1))
+
+            last_end = parts[1]
+
+    def __iter__(self):
+        return itertools.chain(*self.ranges)
+
 
 
--- tests/test_multirange.py
+++ tests/test_multirange.py
@@ -4,5 +4,25 @@ from extractframes.multirange import *
 
 def testSingleRange():
     assert_equal([0,1,2,3,4], list(multirange('0-4')))
+
+def testTwoRange():
+    s = '0-2,10-12'
+    assert_equal([0,1,2,10,11,12], list(multirange(s)))
     
+def testThreeRange():
+    s = '0-2,20-25,30-31'
+    assert_equal([0,1,2,20,21,22,23,24,25,30,31], list(multirange(s)))
+
+@raises(ValueError)
+def testErrorOverlappingRanges():
+    s = '0-10,5-15'
+    list(multirange(s))
+
+
+def testIsType():
+    assert_equal(type(multirange), type)
+
+#def testFloatParse():
+    #s = '0.5-0.8'
+
 
